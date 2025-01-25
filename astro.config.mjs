2d78a4e9fc9aa18bd3bf870b989a20f3e083bf37
Yuniel Acosta Pérez <33158051+yacosta738@users.noreply.github.com>
2025-01-25 23:39:24 +0100

    feat: ✨ Add landing page, hero section, and feature cards; integrate Tailwind CSS and expressive code

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,28 @@ import { defineConfig, envField } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import tailwind from "@astrojs/tailwind";
+
+import expressiveCode from "astro-expressive-code";
+
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
-	env: {
-		schema: {
-			RESEND_API_KEY: envField.string({ context: "server", access: "secret" }),
-			SUPABASE_URL: envField.string({ context: "server", access: "secret" }),
-			SUPABASE_KEY: envField.string({ context: "server", access: "secret" }),
+  output: "server",
 
-		}
-	}
+  adapter: cloudflare({
+      platformProxy: {
+          enabled: true,
+      },
+    }),
+
+  env: {
+      schema: {
+          RESEND_API_KEY: envField.string({ context: "server", access: "secret" }),
+          SUPABASE_URL: envField.string({ context: "server", access: "secret" }),
+          SUPABASE_KEY: envField.string({ context: "server", access: "secret" }),
+
+      }
+    },
+
+  integrations: [tailwind(), expressiveCode()]
 });
